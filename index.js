343ac1e00ad3d24cfa34b56b363779f284b15162
System Administrator <root@Sandros-MacBook-Pro.local>
2021-12-25 17:49:07 -0500

    change start script

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
+
+app.use(express.json());
+app.listen(port, () => console.log('Server lsitening on port ' + port));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/status.html');
 });
-
-app.listen(PORT, () => console.log('Server lsitening on port ' + PORT));
