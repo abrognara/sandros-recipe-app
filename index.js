4be5c846b7fabfc4765d5a4c51d4b85c87cea66c
System Administrator <root@Sandros-MacBook-Pro.local>
2021-12-25 20:24:36 -0500

    fixing start script

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
-app.listen(port, () => console.log('Server lsitening on port ' + port));
+app.listen(port, () => console.log('Server listening on port ' + port));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     res.sendFile(__dirname + '/status.html');
+});
+
+app.get('/recipe', async (req, res) => {
+    res.json({ message: 'Sample Recipe' });
 });
